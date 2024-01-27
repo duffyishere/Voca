ec18fb9282699b0890476545f3653be86335d4c2
zzunho9 <jamlove4763@naver.com>
2024-01-27 21:39:32 +0900

    Initial Commit

---
+++ Vocabulary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vocabulary
+//
+//  Created by Junho's Macbook pro on 1/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vocabulary/VocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyApp.swift
+//  Vocabulary
+//
+//  Created by Junho's Macbook pro on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
