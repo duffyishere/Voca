b91f652591c33d43f5588eb10854231799f0e25e
zzunho9 <jamlove4763@naver.com>
2024-02-01 12:01:59 +0900

    Chore: Add theme files

--- Vocabulary/Models/Vacabulary.swift
+++ Vocabulary/Models/Vacabulary.swift
@@ -17,7 +17,7 @@ extension Vocabulary {
     static let sampleData: [Vocabulary] =
     [
         Vocabulary(name: "Day 20", numberOfWord: 20, theme: .yellow),
-        Vocabulary(name: "Day 21", numberOfWord: 13, theme: .purple),
-        Vocabulary(name: "Day 22", numberOfWord: 24, theme: .buttercup)
+        Vocabulary(name: "Day 21", numberOfWord: 13, theme: .orange),
+        Vocabulary(name: "Day 22", numberOfWord: 24, theme: .poppy)
     ]
 }
---
+++ Vocabulary/VocabulariesView.swift
@@ -0,0 +1,8 @@
+//
+//  VocabulariesView.swift
+//  Vocabulary
+//
+//  Created by Junho's Macbook pro on 2/1/24.
+//
+
+import Foundation
