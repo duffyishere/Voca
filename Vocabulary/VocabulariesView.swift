7ef38f868f6187bac4161098efd8f71f5ad9df39
zzunho9 <jamlove4763@naver.com>
2024-02-03 18:51:42 +0900

    Feat: Add Vocabulary list view

--- Vocabulary/VocabulariesView.swift
+++ Vocabulary/VocabulariesView.swift
@@ -1,8 +1,25 @@
+
 //
 //  VocabulariesView.swift
 //  Vocabulary
 //
-//  Created by Junho's Macbook pro on 2/1/24.
+//  Created by 곽준호 on 2/3/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct VocabulariesView: View {
+    let vocabularies: [Vocabulary]
+    var body: some View {
+        List(vocabularies, id: \.name) { vocabulary in
+            CardView(vocabulary: vocabulary)
+                .listRowBackground(vocabulary.theme.mainColor)
+        }
+    }
+}
+
+struct VocabulariesView_Preview: PreviewProvider {
+    static var previews: some View {
+        VocabulariesView(vocabularies: Vocabulary.sampleData)
+    }
+}
