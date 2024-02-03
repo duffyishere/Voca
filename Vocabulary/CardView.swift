35840a5851bfc85706477cfa2a9d3e9deaf8c8d8
zzunho9 <jamlove4763@naver.com>
2024-02-03 18:42:36 +0900

    Feat: Add Vocabulary list view

--- Vocabulary/CardView.swift
+++ Vocabulary/CardView.swift
@@ -8,22 +8,22 @@
 import SwiftUI
 
 struct CardView: View {
-    let voca: Vocabulary
+    let vocabulary: Vocabulary
     var body: some View {
         VStack(alignment: .leading, content: {
-            Text(voca.name)
+            Text(vocabulary.name)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .font(.headline)
         })
         .padding()
-        .foregroundStyle(voca.theme.accentColor)
+        .foregroundStyle(vocabulary.theme.accentColor)
     }
 }
 
 struct CardView_Previews: PreviewProvider {
     static var voca = Vocabulary.sampleData[0]
     static var previews: some View {
-        CardView(voca: voca)
+        CardView(vocabulary: voca)
             .background(voca.theme.mainColor)
             .previewLayout(.fixed(width: 400, height: 60))
     }
